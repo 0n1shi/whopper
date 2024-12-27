3ea7db03c0974f82cbaaca00dcb8cf44cc2d8025
Kazuki Onishi <princeontrojanhorse@gmail.com>
2024-12-27 23:31:58 +0900

    chore: format output

--- printer/printer_text.go
+++ printer/printer_text.go
@@ -16,7 +16,7 @@ func NewTextPrinter() *TextPrinter {
 func (p *TextPrinter) Print(results []*analyzer.Result) {
 	fmt.Println()
 	for _, result := range results {
-		fmt.Printf("[+] %s", result.Name)
+		fmt.Printf("[+] %s:", result.Name)
 		fmt.Printf(" %s\n", strings.Join(result.Versions, ", "))
 		fmt.Printf("    > %s\n", result.Description)
 		fmt.Println()
