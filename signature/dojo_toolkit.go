d12532b3221557ff5fab40a457de6c138718a350
Kazuki Onishi <princeontrojanhorse@gmail.com>
2025-01-23 23:51:09 +0900

    feat: add signature for dojo toolkit

---
+++ signature/dojo_toolkit.go
@@ -0,0 +1,32 @@
+package signature
+
+import (
+	"regexp"
+
+	"github.com/0n1shi/whopper/crawler"
+)
+
+var DojoToolkitSignature = Signature{
+	Name:        "Dojo Toolkit",
+	Description: "A fast, small, and feature-rich JavaScript library.",
+	Cpe:         "cpe:/a:dojo:dojo_toolkit",
+
+	DetectPattern: DetectPattern{
+		Urls: []string{
+			"dojo.js",
+		},
+		Bodies: []string{
+			"dojotoolkit",
+			"dojo.version",
+		},
+	},
+	VersionFuncs: []VersionFunc{
+		func(res *crawler.Response) string {
+			matches := regexp.MustCompile(`dojo\.version={major:(\d+),minor:(\d+),patch:(\d+),`).FindStringSubmatch(res.Body)
+			if len(matches) > 3 {
+				return matches[1] + "." + matches[2] + "." + matches[3]
+			}
+			return ""
+		},
+	},
+}
---
+++ signature/dojo_toolkit__test.go
@@ -0,0 +1,32 @@
+package signature
+
+import (
+	"testing"
+
+	"github.com/0n1shi/whopper/crawler"
+)
+
+func TestDojoToolkitSignature(t *testing.T) {
+	cases := []TestCase{{
+		name:     "No body and no url",
+		response: &crawler.Response{},
+		detected: false,
+		version:  "",
+	}, {
+		name: "URL",
+		response: &crawler.Response{
+			Url: "http://example.com/dojo/dojo.js",
+		},
+		detected: true,
+		version:  "",
+	}, {
+		name: "Body",
+		response: &crawler.Response{
+			Body: "dojo.version={major:1,minor:6,patch:1,",
+		},
+		detected: true,
+		version:  "1.6.1",
+	}}
+
+	runTests(t, cases, &DojoToolkitSignature)
+}
