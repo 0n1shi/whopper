412f42bd9a50fd813fed2ae1aef49d7e83f81de3
Kazuki Onishi <princeontrojanhorse@gmail.com>
2025-01-31 15:25:52 +0900

    feat: version v0.5.2

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-const version = "v0.5.1"
+const version = "v0.5.2"
 
 func main() {
 	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})))
