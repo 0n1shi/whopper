15b745196f76344c1ca1fe1f331124ebc6b18a3e
Kazuki Onishi <princeontrojanhorse@gmail.com>
2025-02-03 09:24:39 +0900

    test: check that every signature has at least one detect pattern

---
+++ signature/signature__test.go
@@ -0,0 +1,14 @@
+package signature
+
+import (
+	"testing"
+)
+
+func TestSignatures(t *testing.T) {
+	for _, s := range Signatures {
+		// Check if the signature has at least one detect pattern
+		if len(s.DetectPattern.Urls) == 0 && len(s.DetectPattern.Bodies) == 0 && len(s.DetectPattern.Headers) == 0 && len(s.DetectPattern.Cookies) == 0 {
+			t.Errorf("Signature %s has no detect pattern", s.Name)
+		}
+	}
+}
