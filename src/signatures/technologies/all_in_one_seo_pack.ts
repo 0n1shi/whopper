2b01c312756c53d8c8be4c9c6c0e89464005056f
Kazuki Onishi <princeontrojanhorse@gmail.com>
2026-01-06 18:41:01 +0900

    feat: change cpe 2.2 format

--- src/signatures/technologies/all_in_one_seo_pack.ts
+++ src/signatures/technologies/all_in_one_seo_pack.ts
@@ -5,7 +5,7 @@ export const allInOneSeoPackSignature: Signature = {
   name: "All in One SEO Pack",
   description:
     "All in One SEO plugin optimizes WordPress website and its content for search engines.",
-  cpe: "cpe:2.3:a:aioseo:all_in_one_seo:*:*:*:*:*:wordpress:*:*",
+  cpe: "cpe:/a:aioseo:all_in_one_seo",
   rule: {
     confidence: "high",
     bodies: ["<!-- All in One SEO Pack ([\\d.]+) "],
